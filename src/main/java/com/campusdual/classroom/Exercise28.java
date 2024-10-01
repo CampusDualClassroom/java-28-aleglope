b09aad4a3a7ed4a48f740412973663295eff734e
AGonzL <agonzlopez.11@gmail.com>
2024-10-01 11:05:52 +0200

    Ejercicio 28 completado

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -2,6 +2,7 @@ package com.campusdual.classroom;
 
 public class Exercise28 {
     public static void main(String[] args) {
-
+        // Lanzar una excepción de tipo RuntimeException
+        throw new RuntimeException("Este es un lanzamiento intencionado de una excepción.");
     }
 }
